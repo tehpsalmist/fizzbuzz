eee73494c469009b01a58080a63179939d7b5171
tehpsalmist <bstewardcodes@gmail.com>
2018-03-21 13:55:16 -0400

    moar variable elimination

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-const fizz = function () {
-	console.log('fizz')
-}
-const buzz = function () {
-	console.log('buzz')
-}
-const fizzBuzz = function () {
-	console.log('fizz buzz')
-}
 let num = 0
 while (num < 101) {
-	num % 5 === 0 && num % 3 === 0 ? fizzBuzz() : num % 3 === 0 ? fizz() : num % 5 === 0 ? buzz() : console.log(num)
+	num % 5 === 0 && num % 3 === 0 ? console.log('fizz buzz') : num % 3 === 0 ? console.log('fizz') : num % 5 === 0 ? console.log('buzz') : console.log(num)
 	num++
 }
